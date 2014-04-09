fe8929ddb4ebadbd683b4c40b06af04ba4ad3b1f
dragonpig <mikezhuyuan@hotmail.com>
2014-04-09 18:08:57 +1000

    Update main.js

--- main.js
+++ main.js
@@ -224,7 +224,7 @@ angular
 			tags: $scope.tags && $scope.tags.split(/\s+/),
 			eventType: $scope.eventType,
 			eventScheduleType: $scope.eventScheduleType,
-			eventScheduleValue: $scope.eventScheduleType == 'weekly' ? $scope.eventWeeklyOption : $scope.eventMonthlyOption
+			eventScheduleOption: $scope.eventScheduleType == 'weekly' ? $scope.eventWeeklyOption : $scope.eventMonthlyOption
 		}).then(reset);
 	};
 });
